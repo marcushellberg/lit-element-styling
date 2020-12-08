08d06bbd4d1e8166fac8e84ba018662a1a92f866
Marcus Hellberg <marcus@vaadin.com>
2020-12-08 14:39:47 -0800

    base

---
+++ frontend/index.ts
@@ -0,0 +1,46 @@
+import { Flow } from '@vaadin/flow-frontend/Flow';
+import { Router } from '@vaadin/router';
+import './global-styles';
+
+const { serverSideRoutes } = new Flow({
+  imports: () => import('../target/frontend/generated-flow-imports'),
+});
+
+const routes = [
+  // for client-side, place routes below (more info https://vaadin.com/docs/v15/flow/typescript/creating-routes.html)
+  {
+    path: '',
+    component: 'main-view',
+    action: async () => {
+      await import('./views/main/main-view');
+    },
+    children: [
+      {
+        path: '',
+        component: 'one-view',
+        action: async () => {
+          await import('./views/one/one-view');
+        },
+      },
+      {
+        path: 'one',
+        component: 'one-view',
+        action: async () => {
+          await import('./views/one/one-view');
+        },
+      },
+      {
+        path: 'two',
+        component: 'two-view',
+        action: async () => {
+          await import('./views/two/two-view');
+        },
+      },
+      // for server-side, the next magic line sends all unmatched routes:
+      ...serverSideRoutes, // IMPORTANT: this must be the last entry in the array
+    ],
+  },
+];
+
+export const router = new Router(document.querySelector('#outlet'));
+router.setRoutes(routes);
