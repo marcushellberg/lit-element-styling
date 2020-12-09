bb4ec6d081b88c5139dbc9c1e9faa50e44b64240
Marcus Hellberg <marcus@vaadin.com>
2020-12-08 16:38:03 -0800

    remove css module stuff

--- types.d.ts
+++ types.d.ts
@@ -3,13 +3,3 @@ declare module "*.css" {
   const content: CSSResult;
   export default content;
 }
-
-// declare module "*.module.css" {
-//   const classes: { [key: string]: string };
-//   export default classes;
-// }
-
-// declare module "*.css" {
-//   const content: string;
-//   export default content;
-// }
